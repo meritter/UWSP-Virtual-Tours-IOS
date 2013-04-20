4b883df6bbf495eb87c3b812533b1d553f3f51a5
Jonathan Christian <jcisinthehouse21@gmail.com>
2013-04-20 17:49:59 -0500

    fixed poi class

---
+++ Poi.h
@@ -0,0 +1,18 @@
+//
+//  UWSP Virtual Tours
+//  Poi.h
+//  Created by Jonathan Christian on 2/18/13.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Poi : NSObject
+
+@property (nonatomic, retain) NSString * title;
+@property (nonatomic, retain) NSString * description;
+@property int poiId;
+@property double lat;
+@property double lon;
+@property bool visited;
+
+@end
---
+++ Poi.m
@@ -0,0 +1,13 @@
+//
+//  UWSP Virtual Tours
+//  Poi.m
+//  Created by Jonathan Christian on 2/18/13.
+//
+
+#import "Poi.h"
+
+@implementation Poi
+
+@synthesize title, description, poiId, lon, lat, visited;
+
+@end
